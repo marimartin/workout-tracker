6bb66b26b80db9b0c49a762968f191bc9d0df927
Mari <marinarosemartin@gmail.com>
2020-10-28 21:36:28 -0500

    created mongoose schema

---
+++ models/workout.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: { type: Date, default: new Date() },
+    exercises: [
+        {
+            type: { type: String, required: true },
+            name: { type: String, required: true },
+            duration: { type: Number, required: false },
+            weight: { type: Number, required: false },
+            reps: { type: Number, required: false },
+            sets: { type: Number, required: false }
+        }
+    ]
+})
+
+module.exports = mongoose.model('Workout', workoutSchema);
